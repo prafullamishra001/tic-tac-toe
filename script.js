b3075da1c90b0ce16e209c6eed4b4dcbdd335726
Prafulla Mishra <prafullamishra45@gmail.com>
2025-11-03 21:23:42 +0530

    winner logic done

--- script.js
+++ script.js
@@ -1,12 +1,31 @@
 let currentplayer="X"
-let array=Array(9).fill(null)
+let arr=Array(9).fill(null)
 
-
+function checkwinner(){
+if(
+    ( arr[0]!==null && arr[0]===arr[1] && arr[1]===arr[2]) ||
+    ( arr[3]!==null && arr[3]===arr[4] && arr[4]===arr[5]) ||
+    ( arr[6]!==null && arr[6]===arr[7] && arr[7]===arr[8]) ||
+    ( arr[0]!==null && arr[0]===arr[3] && arr[3]===arr[6]) ||
+    ( arr[1]!==null && arr[1]===arr[4] && arr[4]===arr[7]) ||
+    ( arr[2]!==null && arr[2]===arr[5] && arr[5]===arr[8]) ||
+    ( arr[0]!==null && arr[0]===arr[4] && arr[4]===arr[8]) ||
+    ( arr[2]!==null && arr[2]===arr[4] && arr[4]===arr[6])
+){
+document.write(`Winner is ${currentplayer}`)
+return;
+}
+if(!arr.some(e=>e===null)){
+    document.write(`It is a Draw`)
+}
+}
 
 function handleclick(el){
     const id=Number(el.id);
-    array[id]=currentplayer
+    if(arr[id]!==null)return;
+    arr[id]=currentplayer
     el.innerText=currentplayer;
+    checkwinner();
     currentplayer = currentplayer === "X"  ? "O":"X"
-    console.log(array)
+   
 }
