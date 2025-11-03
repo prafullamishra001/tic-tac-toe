f1a52900d89b229a274f5b91606513f5ee1d73e0
Prafulla Mishra <prafullamishra45@gmail.com>
2025-11-03 20:58:44 +0530

    X,O logic done

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-function handleclick(){}
+let currentplayer="X"
+let array=Array(9).fill(null)
+
+
+
+function handleclick(el){
+    const id=Number(el.id);
+    array[id]=currentplayer
+    el.innerText=currentplayer;
+    currentplayer = currentplayer === "X"  ? "O":"X"
+    console.log(array)
+}
